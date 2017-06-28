37bba46ef33b0010e4dedcbc928dc6a8d13d68e7
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2017-06-28 10:46:46 -0500

    se agrega opción para añadir nuevo concepto

--- app/scripts/controllers/conceptos/modulo_conceptos.js
+++ app/scripts/controllers/conceptos/modulo_conceptos.js
@@ -10,11 +10,12 @@
 angular.module('titanClienteV2App')
   .controller('ConceptoConceptosConsultaCtrl', function (titanRequest,$window,$scope,$translate) {
     var self = this;
-    self.id;
-    self.alias_concepto;
-    self.nombre_concepto;
-    self.naturaleza;
-    self.tipo;
+    self.id_edicion;
+    self.alias_concepto_edicion;
+    self.nombre_concepto_edicion;
+    self.naturaleza_edicion;
+    self.tipo_edicion;
+
   //  self.tipo="porcentaje";
     self.gridOptions_conceptos = {
 
@@ -26,7 +27,7 @@ angular.module('titanClienteV2App')
       columnDefs : [
         {field: 'Id',             visible : false},
         {field: 'NombreConcepto',  visible : false},
-        {field: 'AliasConcepto',  displayName: $translate.instant('CONCEPTO')},
+        {field: 'AliasConcepto',  displayName: $translate.instant('CONCEPTO'), },
         {field: 'Naturaleza',  displayName: $translate.instant('NATURALEZA')},
         {field: 'TipoConcepto',  displayName: 'Tipo'},
         {field: 'Acciones',
@@ -47,11 +48,11 @@ angular.module('titanClienteV2App')
     });
 
     $scope.llenar_modal = function(row){
-      self.id = row.entity.Id
-      self.alias_concepto = row.entity.AliasConcepto
-      self.nombre_concepto = row.entity.NombreConcepto
-      self.naturaleza = row.entity.Naturaleza
-      self.tipo = row.entity.TipoConcepto
+      self.id_edicion = row.entity.Id
+      self.alias_concepto_edicion = row.entity.AliasConcepto
+      self.nombre_concepto_edicion = row.entity.NombreConcepto
+      self.naturaleza_edicion = row.entity.Naturaleza
+      self.tipo_edicion = row.entity.TipoConcepto
     };
 
     self.actualizar = function() {
@@ -59,9 +60,9 @@ angular.module('titanClienteV2App')
       swal({
              html:
              "¿Está seguro de querer actualizar el concepto:"+
-             "<br><b>Nombre:</b> "+self.alias_concepto+
-             "<br><b>Naturaleza:</b> "+self.naturaleza+
-             "<br><b>Tipo:</b> "+self.tipo+"?",
+             "<br><b>Nombre:</b> "+self.alias_concepto_edicion+
+             "<br><b>Naturaleza:</b> "+self.naturaleza_edicion+
+             "<br><b>Tipo:</b> "+self.tipo_edicion+"?",
              type: "warning",
              showCancelButton: true,
              confirmButtonColor: "#449D44",
@@ -71,11 +72,11 @@ angular.module('titanClienteV2App')
            }).then(function() {
 
              var concepto_editado = {
-                   Id : self.id,
-                   NombreConcepto: self.nombre_concepto,
-                   Naturaleza: self.naturaleza,
-                   AliasConcepto: self.alias_concepto,
-                   TipoConcepto: self.tipo
+                   Id : self.id_edicion,
+                   NombreConcepto: self.nombre_concepto_edicion,
+                   Naturaleza: self.naturaleza_edicion,
+                   AliasConcepto: self.alias_concepto_edicion,
+                   TipoConcepto: self.tipo_edicion
                   };
                   titanRequest.put('concepto', concepto_editado.Id,concepto_editado).then(function(response) {
                     console.log(response.data);
@@ -157,4 +158,78 @@ angular.module('titanClienteV2App')
               }
             })
           };
+
+
+          self.anadir = function() {
+
+            swal({
+                   html:
+                   "¿Está seguro de querer añadir el concepto:"+
+                   "<br><b>Nombre:</b> "+self.alias_concepto_adicion+
+                   "<br><b>Naturaleza:</b> "+self.naturaleza_adicion+
+                   "<br><b>Tipo:</b> "+self.tipo_adicion+"?",
+                   type: "warning",
+                   showCancelButton: true,
+                   confirmButtonColor: "#449D44",
+                   cancelButtonColor: "#C9302C",
+                   confirmButtonText: "Confirmar",
+                   cancelButtonText: "Cancelar",
+                 }).then(function() {
+
+                   var concepto_nuevo_temp = {
+
+                         NombreConcepto: "nombrereglaxxx",
+                         Naturaleza: self.naturaleza_adicion,
+                         AliasConcepto: self.alias_concepto_adicion,
+                         TipoConcepto: self.tipo_adicion
+                        };
+
+                        console.log(concepto_nuevo_temp)
+                        titanRequest.post('concepto', concepto_nuevo_temp).then(function(response) {
+                          console.log(response.data);
+                          if(typeof(response.data)=="object"){
+
+                            var concepto_nuevo = {
+                                  Id: response.data.Id,
+                                  NombreConcepto: "nombreregla" + response.data.Id.toString(),
+                                  Naturaleza: response.data.Naturaleza,
+                                  AliasConcepto: response.data.AliasConcepto,
+                                  TipoConcepto: response.data.TipoConcepto
+                            };
+
+                            titanRequest.put('concepto', concepto_nuevo.Id,concepto_nuevo).then(function(response) {
+                              console.log(response.data);
+                              if(response.data=="OK"){
+                                swal({
+                                   html: "Registrado correctamente",
+                                   type: "success",
+                                   showCancelButton: false,
+                                   confirmButtonColor: "#449D44",
+                                   confirmButtonText: $translate.instant('VOLVER'),
+                                   }).then(function() {
+                                  $window.location.href = '#/conceptos/modulo_conceptos';
+                                 })
+                              }
+                              else{
+                                swal({
+                                   html: "Error",
+                                   type: "error",
+                                   showCancelButton: false,
+                                   confirmButtonColor: "#449D44",
+                                   confirmButtonText: $translate.instant('VOLVER'),
+                                   }).then(function() {
+                                  $window.location.href = '#/conceptos/modulo_conceptos';
+                                 })
+                              }
+                            });
+                          }
+                        });
+
+                 }, function(dismiss) {
+                   if (dismiss === 'cancel') {
+
+                     $window.location.href = '#/conceptos/modulo_conceptos';
+                   }
+                 })
+           };
   });
