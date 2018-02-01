d63ace10336051bbde02e80e0cbc5c12e2e05023
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2018-02-01 17:39:28 -0500

    cambios locales

--- app/scripts/services/token.js
+++ app/scripts/services/token.js
@@ -2,16 +2,16 @@
 
 /**
  * @ngdoc service
- * @name prototipoApp.token
+ * @name titanClienteV2App.token
  * @description
  * # token
- * Factory in the prototipoApp.
+ * Factory in the titanClienteV2App.
  */
 
 
 // First, parse the query string
 var params = {},
-    queryString = location.hash.substring(1),
+    queryString = location.search.substring(1),
     regex = /([^&=]+)=([^&]*)/g,
     m;
 while (!!(m = regex.exec(queryString))) {
@@ -23,7 +23,7 @@ var req = new XMLHttpRequest();
 var query = 'https://' + window.location.host + '?' + queryString;
 //console.log(query);
 req.open('GET', query, true);
-
+if (params.code !== undefined) {}
 req.onreadystatechange = function(e) {
     console.log(e);
     if (req.readyState === 4) {
@@ -39,13 +39,20 @@ req.onreadystatechange = function(e) {
 };
 
 angular.module('titanClienteV2App')
-    .factory('token_service', function($location, $http, $localStorage, CONF) {
+    .factory('token_service', function($location, $http, $sessionStorage, CONF, $interval) {
+
         var service = {
-            local: $localStorage.$default(params),
-            //session: $sessionStorage.default(params),
+            session: $sessionStorage.$default(params),
             header: null,
             token: null,
-
+            setting_basic: {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded",
+                    "authorization": "Basic " + btoa(CONF.GENERAL.TOKEN.CLIENTE_ID + ":" + CONF.GENERAL.TOKEN.CLIENT_SECRET),
+                    "cache-control": "no-cache",
+                }
+            },
+            setting_bearer: null,
             config: {
                 AUTORIZATION_URL: CONF.GENERAL.TOKEN.AUTORIZATION_URL,
                 CLIENTE_ID: CONF.GENERAL.TOKEN.CLIENTE_ID,
@@ -59,25 +66,91 @@ angular.module('titanClienteV2App')
             },
 
             live_token: function() {
-                if (typeof service.local.id_token === 'undefined' || service.local.id_token === null) {
+                if (service.session === null) {
+                    service.session = $sessionStorage.$default(params);
                     return false;
                 } else {
-                    service.header = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(service.local.id_token.split(".")[0]));
-                    service.token = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(service.local.id_token.split(".")[1]));
-                    return true;
+                    if (!angular.isUndefined(service.session.id_token)) {
+                        service.header = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(service.session.id_token.split(".")[0]));
+                        service.token = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(service.session.id_token.split(".")[1]));
+                        service.setting_bearer = {
+                            headers: {
+                                "content-type": "application/x-www-form-urlencoded",
+                                "authorization": "Bearer " + $sessionStorage.access_token,
+                                "cache-control": "no-cache",
+                            }
+                        };
+                        return true;
+                    } else {}
                 }
             },
             logout: function() {
                 window.location = $location.absUrl();
                 var url = service.config.SIGN_OUT_URL;
-                url = url + '?logout_redirect_uri=' + service.config.SIGN_OUT_REDIRECT_URL;
-                url = url + '?id_token_hint=' + service.local.id_token;
-                console.log(url);
+                url = url + '?id_token_hint=' + service.session.id_token;
+                url = url + '&post_logout_redirect_uri=' + CONF.GENERAL.TOKEN.SIGN_OUT_REDIRECT_URL;
                 service.token = null;
-                $localStorage.$reset();
+                $sessionStorage.$reset();
                 window.location.replace(url);
+            },
+            refresh: function() {
+                var url = CONF.GENERAL.TOKEN.REFRESH_TOKEN;
+                var data = {};
+                url += "?grant_type=refresh_token";
+                url += "&refresh_token=" + $sessionStorage.refresh_token;
+                url += "&redirect_uri=" + CONF.GENERAL.TOKEN.REDIRECT_URL;
+
+                $http.post(url, data, service.setting_basic)
+                    .then(function(response) {
+                        $sessionStorage.access_token = response.data.access_token;
+                        $sessionStorage.expires_in = response.data.expires_in;
+                        $sessionStorage.id_token = response.data.id_token;
+                        $sessionStorage.refresh_token = response.data.refresh_token;
+                        $sessionStorage.expires_at = null;
+                        service.setExpiresAt();
+                    });
+            },
+            get_id_token: function() {
+                if ((!angular.isUndefined($sessionStorage.code)) && (angular.isUndefined($sessionStorage.id_token))) {
+                    var url = CONF.GENERAL.TOKEN.REFRESH_TOKEN;
+                    var data = {};
+                    url += "?grant_type=authorization_code";
+                    url += "&code=" + $sessionStorage.code;
+                    url += "&redirect_uri=" + CONF.GENERAL.TOKEN.REDIRECT_URL;
+
+                    $http.post(url, data, service.setting_basic)
+                        .then(function(response) {
+                            window.location.replace(CONF.GENERAL.TOKEN.REDIRECT_URL);
+                            $sessionStorage.$default(response.data);
+                            service.timer();
+                            service.setExpiresAt();
+                        });
+                }
+
+            },
+            setExpiresAt: function() {
+                if (angular.isUndefined($sessionStorage.expires_at) || $sessionStorage.expires_at === null) {
+                    var expires_at = new Date();
+                    expires_at.setSeconds(expires_at.getSeconds() + parseInt($sessionStorage.expires_in) - 60); // 60 seconds less to secure browser and response latency
+                    $sessionStorage.expires_at = expires_at;
+                }
+            },
+            expired: function() {
+                return (new Date($sessionStorage.expires_at) < new Date());
+            },
+
+            timer: function() {
+                if (!angular.isUndefined($sessionStorage.expires_at) || $sessionStorage.expires_at === null) {
+                    $interval(function() {
+                        if (service.expired()) {
+                            service.refresh();
+                        }
+                    }, 5000);
+                }
             }
 
         };
+        //
+        service.get_id_token();
         return service;
     });
