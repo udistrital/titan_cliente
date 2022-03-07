5a35ff7ace22fb066472bcbabd1b858e84a23653
jorgec815 <jorgecortes815@gmail.com>
2022-03-06 22:24:14 -0500

    Feat: Actualizar preliquidado por botón

--- app/scripts/directives/preliquidacion/reportes_spagobi.js
+++ app/scripts/directives/preliquidacion/reportes_spagobi.js
@@ -7,31 +7,31 @@
 * # reportesSpagobi
 */
 angular.module('titanClienteV2App')
-  .directive('spagobi', function () {
-    return {
-      restrict: "E",
-      scope:{ 
-          reporte: "@",
-          resolucion: "@",
-          dependencia: "=",
-          vigencia: "=",
-          mes: "=",
-          boton: "="
-      },
-      template: '<div id="frame" ></div>',
-      controller:function($scope){
-       
-        $scope.$watch('reporte', function (newValue, oldValue) {
-            
-          if ($scope.reporte && $scope.reporte.length !== 0) {
-           
-            url="https://inteligenciainstitucional.portaloas.udistrital.edu.co/knowage/servlet/AdapterHTTP?ACTION_NAME=EXECUTE_DOCUMENT_ACTION&TOOLBAR_VISIBLE=true&ORGANIZATION=DEFAULT_TENANT&NEW_SESSION=true&OBJECT_LABEL="+RteTitan
-              $('#frame').html('');
-              $('#frame').append(url);
-            
-          }
-        });
-      },
-      controllerAs:'d_reportesSpagobi'
-    };
-  });
+	.directive('spagobi', function () {
+		return {
+			restrict: "E",
+			scope: {
+				reporte: "@",
+				resolucion: "@",
+				dependencia: "=",
+				vigencia: "=",
+				mes: "=",
+				boton: "="
+			},
+			template: '<div id="frame" ></div>',
+			controller: function ($scope) {
+
+				$scope.$watch('reporte', function (newValue, oldValue) {
+
+					if ($scope.reporte && $scope.reporte.length !== 0) {
+
+						url = "https://inteligenciainstitucional.portaloas.udistrital.edu.co/knowage/servlet/AdapterHTTP?ACTION_NAME=EXECUTE_DOCUMENT_ACTION&TOOLBAR_VISIBLE=true&ORGANIZATION=DEFAULT_TENANT&NEW_SESSION=true&OBJECT_LABEL=" + RteTitan
+						$('#frame').html('');
+						$('#frame').append(url);
+
+					}
+				});
+			},
+			controllerAs: 'd_reportesSpagobi'
+		};
+	});
